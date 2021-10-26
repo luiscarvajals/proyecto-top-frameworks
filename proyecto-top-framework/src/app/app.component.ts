d411bc2b361c160e5190dbb36633dba6a20820fa
Luis Carvajal <luiscarvajalsanchez2@gmail.com>
2021-10-25 23:53:36 -0400

    final

--- proyecto-top-framework/src/app/app.component.ts
+++ proyecto-top-framework/src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-top-framework';
+  top1 = "RUBY ON RAILS";
+  top1img = "https://www.monocubed.com/wp-content/uploads/2021/02/Ruby-on-rails-1024x459.png";
+  top1desc = "‎Ruby on Rails‎‎ es un marco de aplicación web dinámico, perfecto para desarrollar una aplicación de alta velocidad. Descubiertas por David Heinemeier Hansson, las aplicaciones de Ruby on Rails son generalmente diez veces más rápidas. Es uno de los mejores marcos de backend, ya ‎‎que comprende todo lo que se necesita para formar una ‎‎ ‎‎ ‎‎aplicación basada en bases de datos.‎ Los sitios web que han incorporado Ruby on Rails son GitHub, Airbnb, GroupOn, Shopify, Hulu, por nombrar algunos. No solo esto, sino que también puede explorar ‎‎las principales compañías que usan Ruby on Rails‎‎ para sus sitios web y aplicaciones.‎";
+  top1link = "http://rubyonrails.org/";
+
+  top2 = "LARAVEL";
+  top2img = "https://www.monocubed.com/wp-content/uploads/2021/02/Laravel-1024x501.png";
+  top2desc = "‎Simples, elegantes y legibles son los USP de Larval. Taylor Otwell desarrolló Laravel en 2011. Al igual que otros marcos de desarrollo contemporáneos, funciona en un modelo arquitectónico MVC que utiliza PHP.‎ ‎Laravel tiene soporte api y contiene una ‎‎buena cantidad de paquetes‎‎ que podrían ampliar su alcance. ‎‎ ‎‎Laracasts‎‎ ‎‎ es un sitio web tutorial con miles de videos sobre Laravel, PHP y tecnologías frontend en el ecosistema de Laravel. ‎‎Laravel incluye muchas características como la inyección de dependencias, la representación del lado del servidor Está demostrando ser el mejor marco para sitios web como Neighborhood, Travel, Deltanet, Lender y muchos más.‎";
+  top2link = "https://laravel.com/";
+
+  top3 = "DJANGO";
+  top3img = "https://www.monocubed.com/wp-content/uploads/2021/02/Django-1024x497.png";
+  top3desc = "‎Ruby on Rails‎‎ es un marco de aplicación web dinámico, perfecto para desarrollar una aplicación de alta velocidad. Descubiertas por David Heinemeier Hansson, las aplicaciones de Ruby on Rails son generalmente diez veces más rápidas. Es uno de los mejores marcos de backend, ya ‎‎que comprende todo lo que se necesita para formar una ‎‎ ‎‎ ‎‎aplicación basada en bases de datos.‎ Los sitios web que han incorporado Ruby on Rails son GitHub, Airbnb, GroupOn, Shopify, Hulu, por nombrar algunos. No solo esto, sino que también puede explorar ‎‎las principales compañías que usan Ruby on Rails‎‎ para sus sitios web y aplicaciones.‎";
+  top3link = "http://rubyonrails.org/";
+
+  top4 = "ASP.NET";
+  top4img = "https://www.monocubed.com/wp-content/uploads/2021/02/ASP.net_-1024x508.png";
+  top4desc = "‎Simples, elegantes y legibles son los USP de Larval. Taylor Otwell desarrolló Laravel en 2011. Al igual que otros marcos de desarrollo contemporáneos, funciona en un modelo arquitectónico MVC que utiliza PHP.‎ ‎Laravel tiene soporte api y contiene una ‎‎buena cantidad de paquetes‎‎ que podrían ampliar su alcance. ‎‎ ‎‎Laracasts‎‎ ‎‎ es un sitio web tutorial con miles de videos sobre Laravel, PHP y tecnologías frontend en el ecosistema de Laravel. ‎‎Laravel incluye muchas características como la inyección de dependencias, la representación del lado del servidor Está demostrando ser el mejor marco para sitios web como Neighborhood, Travel, Deltanet, Lender y muchos más.‎";
+  top4link = "http://rubyonrails.org/";
+
+  top5 = "EXPRESS";
+  top5img = "https://www.monocubed.com/wp-content/uploads/2021/02/Express-1024x504.png";
+  top5desc = "‎Ruby on Rails‎‎ es un marco de aplicación web dinámico, perfecto para desarrollar una aplicación de alta velocidad. Descubiertas por David Heinemeier Hansson, las aplicaciones de Ruby on Rails son generalmente diez veces más rápidas. Es uno de los mejores marcos de backend, ya ‎‎que comprende todo lo que se necesita para formar una ‎‎ ‎‎ ‎‎aplicación basada en bases de datos.‎ Los sitios web que han incorporado Ruby on Rails son GitHub, Airbnb, GroupOn, Shopify, Hulu, por nombrar algunos. No solo esto, sino que también puede explorar ‎‎las principales compañías que usan Ruby on Rails‎‎ para sus sitios web y aplicaciones.‎";
+  top5link = "http://rubyonrails.org/";
+
+  top6 = "SPRING";
+  top6img = "https://www.monocubed.com/wp-content/uploads/2021/02/Spring-1024x484.png";
+  top6desc = "‎Simples, elegantes y legibles son los USP de Larval. Taylor Otwell desarrolló Laravel en 2011. Al igual que otros marcos de desarrollo contemporáneos, funciona en un modelo arquitectónico MVC que utiliza PHP.‎ ‎Laravel tiene soporte api y contiene una ‎‎buena cantidad de paquetes‎‎ que podrían ampliar su alcance. ‎‎ ‎‎Laracasts‎‎ ‎‎ es un sitio web tutorial con miles de videos sobre Laravel, PHP y tecnologías frontend en el ecosistema de Laravel. ‎‎Laravel incluye muchas características como la inyección de dependencias, la representación del lado del servidor Está demostrando ser el mejor marco para sitios web como Neighborhood, Travel, Deltanet, Lender y muchos más.‎";
+  top6link = "https://laravel.com/";
+
+  top7 = "ANGULAR";
+  top7img = "https://www.monocubed.com/wp-content/uploads/2021/02/Angular-1024x485.png";
+  top7desc = "‎Ruby on Rails‎‎ es un marco de aplicación web dinámico, perfecto para desarrollar una aplicación de alta velocidad. Descubiertas por David Heinemeier Hansson, las aplicaciones de Ruby on Rails son generalmente diez veces más rápidas. Es uno de los mejores marcos de backend, ya ‎‎que comprende todo lo que se necesita para formar una ‎‎ ‎‎ ‎‎aplicación basada en bases de datos.‎ Los sitios web que han incorporado Ruby on Rails son GitHub, Airbnb, GroupOn, Shopify, Hulu, por nombrar algunos. No solo esto, sino que también puede explorar ‎‎las principales compañías que usan Ruby on Rails‎‎ para sus sitios web y aplicaciones.‎";
+  top7link = "http://rubyonrails.org/";
+
+  top8 = "EMBER";
+  top8img = "https://www.monocubed.com/wp-content/uploads/2021/02/Ember-1024x489.png";
+  top8desc = "‎Simples, elegantes y legibles son los USP de Larval. Taylor Otwell desarrolló Laravel en 2011. Al igual que otros marcos de desarrollo contemporáneos, funciona en un modelo arquitectónico MVC que utiliza PHP.‎ ‎Laravel tiene soporte api y contiene una ‎‎buena cantidad de paquetes‎‎ que podrían ampliar su alcance. ‎‎ ‎‎Laracasts‎‎ ‎‎ es un sitio web tutorial con miles de videos sobre Laravel, PHP y tecnologías frontend en el ecosistema de Laravel. ‎‎Laravel incluye muchas características como la inyección de dependencias, la representación del lado del servidor Está demostrando ser el mejor marco para sitios web como Neighborhood, Travel, Deltanet, Lender y muchos más.‎";
+  top8link = "http://rubyonrails.org/";
+
+  top9 = "METEOR";
+  top9img = "https://www.monocubed.com/wp-content/uploads/2021/02/Meteor-1024x497.png";
+  top9desc = "‎Ruby on Rails‎‎ es un marco de aplicación web dinámico, perfecto para desarrollar una aplicación de alta velocidad. Descubiertas por David Heinemeier Hansson, las aplicaciones de Ruby on Rails son generalmente diez veces más rápidas. Es uno de los mejores marcos de backend, ya ‎‎que comprende todo lo que se necesita para formar una ‎‎ ‎‎ ‎‎aplicación basada en bases de datos.‎ Los sitios web que han incorporado Ruby on Rails son GitHub, Airbnb, GroupOn, Shopify, Hulu, por nombrar algunos. No solo esto, sino que también puede explorar ‎‎las principales compañías que usan Ruby on Rails‎‎ para sus sitios web y aplicaciones.‎";
+  top9link = "http://rubyonrails.org/";
+
+  top10 = "VUE";
+  top10img = "https://www.monocubed.com/wp-content/uploads/2021/02/Vue-1024x493.png";
+  top10desc = "‎Simples, elegantes y legibles son los USP de Larval. Taylor Otwell desarrolló Laravel en 2011. Al igual que otros marcos de desarrollo contemporáneos, funciona en un modelo arquitectónico MVC que utiliza PHP.‎ ‎Laravel tiene soporte api y contiene una ‎‎buena cantidad de paquetes‎‎ que podrían ampliar su alcance. ‎‎ ‎‎Laracasts‎‎ ‎‎ es un sitio web tutorial con miles de videos sobre Laravel, PHP y tecnologías frontend en el ecosistema de Laravel. ‎‎Laravel incluye muchas características como la inyección de dependencias, la representación del lado del servidor Está demostrando ser el mejor marco para sitios web como Neighborhood, Travel, Deltanet, Lender y muchos más.‎";
+  top10link = "https://laravel.com/";
 }
